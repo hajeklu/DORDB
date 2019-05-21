3b23481d499881550c445e714343b48d54486a9f
manas08 <kratkra1@uhk.cz>
2019-05-21 23:08:40 +0200

    Query analysis

--- 08_dotazy_z_bodu_4.sql
+++ 08_dotazy_z_bodu_4.sql
@@ -1,5 +1,5 @@
 SET AUTOTRACE ON;
-spool '08_vysledky_dotazu.txt';
+spool '08_vysledky_dotazu6.txt';
 
 /*
  1. vypsat jména uživatelù, jejichž poèítaèe mají OS macOS (JOIN)
@@ -15,7 +15,7 @@ select * from (select c.computer_name as Název_poèítaèe, b.computer_brand_maker 
 from a_computer c join a_computer_brand b on (c.computer_brand_id=b.computer_brand_id) order by c.last_update_time desc) where rownum < 11;
 --B
 select * from (select c.computer_name as Název_poèítaèe, (SELECT b.computer_brand_maker from a_computer_brand b where c.computer_brand_id=b.computer_brand_id) as Vyrobce, 
-(SELECT  b.compuer_model from a_computer_brand b where c.computer_brand_id=b.computer_brand_id) as Model, c.last_update_time as Datum_poslední_zmìny 
+(SELECT  b.compuer_model from a_computer_brand b where c.computer_brand_id=b.computer_brand_id) as PCModel, c.last_update_time as Datum_poslední_zmìny 
 from a_computer c order by c.last_update_time desc) where rownum < 11;
 
 
@@ -26,26 +26,25 @@ select u.user_first_name as Jmeno, u.user_last_name as Prijmeni, u.user_role as 
 
 
 /*
- 4. vypsat všechny parametry poèítaèe, jeho název, jméno uživatele, název OS, název výrobce a modelu, který je stále v záruce 2 let (JOIN)
+ 4. vypsat všechny parametry poèítaèe, jeho název, jméno uživatele, název OS, název výrobce a modelu, který je stále v záruce (JOIN)
 */
-select * from (select p.extername_gpu as GPU, p.optical_disk_drive as OpticalDiskDrive, p.hdd as HDD, p.processor as Processor, p.ram as RAM, c.computer_name as Nazev, c.last_update_time as UpdateTime,
-u.user_first_name as Jmeno, u.user_last_name as Prijmeni, o.os_name as OS, b.computer_brand_maker as Vyrobce, b.compuer_model as Model
+select p.extername_gpu as GPU, p.optical_disk_drive as OpticalDiskDrive, p.hdd as HDD, p.processor as Processor, p.ram as RAM, c.computer_name as Nazev, c.guarantees as Guarantees,
+u.user_first_name as Jmeno, u.user_last_name as Prijmeni, o.os_name as OS, b.computer_brand_maker as Vyrobce, b.compuer_model as PCModel
 from a_computer_parameters p 
 join a_computer c on (c.computer_parameters_id=p.computer_parameters_id) join a_computer_brand b on (c.computer_brand_id=b.computer_brand_id) join a_user u on (c.user_id=u.user_id) join a_os o on (c.os_id=o.os_id) 
-order by c.computer_name desc) where trunc(SYSDATE - 730) < last_update_time;
+where SYSDATE < c.guarantees ;
 
 
 
 /*
- 5. Vypsani všech uživatelù a seøazení podle poètu poèítaèù (JOIN)
+ 5. Vypsani uživatelù a seøazení podle poètu poèítaèù - prvních 20 (JOIN)
 */
 --A
-select u.user_first_name as Jméno, u.user_last_name as Pøíjmení, count(c.computer_id) as Poèet_poèítaèù from a_user u join a_computer c on (c.user_id=u.user_id) 
-group by u.user_first_name, u.user_last_name order by Poèet_poèítaèù desc;
+select * from (select u.user_first_name as Jméno, u.user_last_name as Pøíjmení, count(c.computer_id) as Poèet_poèítaèù from a_user u left outer join a_computer c on (c.user_id=u.user_id) 
+group by u.user_first_name, u.user_last_name order by Poèet_poèítaèù desc) where rownum < 21;
 --B
-select u.user_first_name as Jméno, u.user_last_name as Pøíjmení, (SELECT count(computer_id) FROM a_computer m where m.user_id=u.user_id) as Poèet_poèítaèù from a_user u 
-order by Poèet_poèítaèù desc;
-
+select * from (select u.user_first_name as Jméno, u.user_last_name as Pøíjmení, (SELECT count(m.computer_id) FROM a_computer m where m.user_id=u.user_id) as Poèet_poèítaèù from a_user u 
+order by Poèet_poèítaèù desc) where rownum < 21;
 
 SET AUTOTRACE OFF;
 spool off;
