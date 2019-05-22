6d4fafc6bd67c9d4b465b66be03f64103b3722d8
manas08 <kratkra1@uhk.cz>
2019-05-22 15:06:00 +0200

    Complete procedures and output

--- 10_balik_a_procedury.sql
+++ 10_balik_a_procedury.sql
@@ -1,65 +1,66 @@
-CREATE OR REPLACE PACKAGE pocitacova_databaze_api AS
-   PROCEDURE new_user (firstname IN VARCHAR2, lastname IN VARCHAR2, userrole IN VARCHAR2, userpassword IN VARCHAR2, login IN VARCHAR2, email IN VARCHAR2);
-   PROCEDURE new_computer_brand (computer_brand_maker IN VARCHAR2, compuer_model IN VARCHAR2);
-   PROCEDURE new_computer_parameters (extername_gpu IN NUMBER, optical_disk_drive IN NUMBER, hdd IN NUMBER, processor IN VARCHAR2, ram IN NUMBER);
-   PROCEDURE new_os (os_name IN VARCHAR2, os_version IN NUMBER);
-   PROCEDURE new_computer (bpcs_sessions IN VARCHAR2, comments IN VARCHAR2, create_time IN DATE, guarantees IN DATE, inventory_number IN NUMBER, last_update_time IN DATE, mac_address IN VARCHAR2, computer_name IN VARCHAR2, serial_number IN VARCHAR2,
-  computer_brand_maker IN VARCHAR2, compuer_model IN VARCHAR2,
-  extername_gpu IN NUMBER, optical_disk_drive IN NUMBER, hdd IN NUMBER, processor IN VARCHAR2, ram IN NUMBER,
-  os_name IN VARCHAR2, os_version IN NUMBER,
-  firstname IN VARCHAR2, lastname IN VARCHAR2, userrole IN VARCHAR2, userpassword IN VARCHAR2, login IN VARCHAR2, email IN VARCHAR2);
+create or replace PACKAGE pocitacova_databaze_api AS
+   PROCEDURE new_user (Ifirstname IN VARCHAR2, Ilastname IN VARCHAR2, Iuserrole IN VARCHAR2, Iuserpassword IN VARCHAR2, Ilogin IN VARCHAR2, Iemail IN VARCHAR2);
+   PROCEDURE new_computer_brand (Icomputer_brand_maker IN VARCHAR2, Icompuer_model IN VARCHAR2);
+   PROCEDURE new_computer_parameters (Iextername_gpu IN NUMBER, Ioptical_disk_drive IN NUMBER, Ihdd IN NUMBER, Iprocessor IN VARCHAR2, Iram IN NUMBER);
+   PROCEDURE new_os (Ios_name IN VARCHAR2, Ios_version IN NUMBER);
+   PROCEDURE new_computer (Ibpcs_sessions IN VARCHAR2, Icomments IN VARCHAR2, Icreate_time IN DATE, Iguarantees IN DATE, Iinventory_number IN VARCHAR2, Ilast_update_time IN DATE, Imac_address IN VARCHAR2, Icomputer_name IN VARCHAR2, Iserial_number IN VARCHAR2,
+  Icomputer_brand_maker IN VARCHAR2, Icompuer_model IN VARCHAR2,
+  Iextername_gpu IN NUMBER, Ioptical_disk_drive IN NUMBER, Ihdd IN NUMBER, Iprocessor IN VARCHAR2, Iram IN NUMBER,
+  Ios_name IN VARCHAR2, Ios_version IN NUMBER,
+  Ifirstname IN VARCHAR2, Ilastname IN VARCHAR2, Iuserrole IN VARCHAR2, Iuserpassword IN VARCHAR2, Ilogin IN VARCHAR2, Iemail IN VARCHAR2);
 END pocitacova_databaze_api;
 /
 
-CREATE OR REPLACE PACKAGE BODY pocitacova_databaze_api AS
-  PROCEDURE new_user (firstname IN VARCHAR2, lastname IN VARCHAR2, userrole IN VARCHAR2, userpassword IN VARCHAR2, login IN VARCHAR2, email IN VARCHAR2) AS
+create or replace PACKAGE BODY pocitacova_databaze_api AS
+  PROCEDURE new_user (Ifirstname IN VARCHAR2, Ilastname IN VARCHAR2, Iuserrole IN VARCHAR2, Iuserpassword IN VARCHAR2, Ilogin IN VARCHAR2, Iemail IN VARCHAR2) AS
     BEGIN
-    if(trim(firstname) is not null and trim(lastname) is not null and trim(firstname) is not null and trim(login) is not null and trim(userpassword) is not null and trim(userrole) is not null and trim(email) is not null) then
-      INSERT INTO A_USER VALUES (seq_User.nextval, firstname, lastname, userrole, userpassword, login, email);
+    if(trim(Ifirstname) is not null and trim(Ilastname) is not null and trim(Ifirstname) is not null and trim(Ilogin) is not null and trim(Iuserpassword) is not null and trim(Iuserrole) is not null and trim(Iemail) is not null) then
+      INSERT INTO A_USER VALUES (seq_User.nextval, Ifirstname, Ilastname, Iuserrole, Iuserpassword, Ilogin, Iemail);
     else
       RAISE_APPLICATION_ERROR(-20000, 'Vyplnte vsechny udaje.');
     end if;
   END new_user;
-  
-  
-  PROCEDURE new_computer_brand (computer_brand_maker IN VARCHAR2, compuer_model IN VARCHAR2) AS
+
+
+
+  PROCEDURE new_computer_brand (Icomputer_brand_maker IN VARCHAR2, Icompuer_model IN VARCHAR2) AS
     BEGIN
-    if(trim(computer_brand_maker) is not null and trim(compuer_model) is not null) then
-      INSERT INTO A_COMPUTER_BRAND VALUES (seq_Brand.nextval, computer_brand_maker, compuer_model);
+    if(trim(Icomputer_brand_maker) is not null and trim(Icompuer_model) is not null) then
+      INSERT INTO A_COMPUTER_BRAND VALUES (seq_Brand.nextval, Icomputer_brand_maker, Icompuer_model);
     else
       RAISE_APPLICATION_ERROR(-20000, 'Chybne udaje vyrobce pocitace.');
     end if;
   END new_computer_brand;
-  
-  
-  PROCEDURE new_computer_parameters (extername_gpu IN NUMBER, optical_disk_drive IN NUMBER, hdd IN NUMBER, processor IN VARCHAR2, ram IN NUMBER) AS
+
+
+  PROCEDURE new_computer_parameters (Iextername_gpu IN NUMBER, Ioptical_disk_drive IN NUMBER, Ihdd IN NUMBER, Iprocessor IN VARCHAR2, Iram IN NUMBER) AS
     BEGIN
-    if(extername_gpu is not null and optical_disk_drive is not null and hdd>0 and trim(processor) is not null and ram>0) then
-      INSERT INTO A_COMPUTER_PARAMETERS VALUES (seq_Parameters.nextval, extername_gpu, optical_disk_drive, hdd, processor, ram);
+    if(Iextername_gpu is not null and Ioptical_disk_drive is not null and Ihdd>0 and trim(Iprocessor) is not null and Iram>0) then
+      INSERT INTO A_COMPUTER_PARAMETERS VALUES (seq_Parameters.nextval, Iextername_gpu, Ioptical_disk_drive, Ihdd, Iprocessor, Iram);
     else
       RAISE_APPLICATION_ERROR(-20000, 'Chybne udaje parametru pocitace.');
     end if;
   END new_computer_parameters;
-  
-  
-  PROCEDURE new_os (os_name IN VARCHAR2, os_version IN NUMBER) AS
+
+
+  PROCEDURE new_os (Ios_name IN VARCHAR2, Ios_version IN NUMBER) AS
     BEGIN
-    if(trim(os_name) is not null and os_version>0) then
-      INSERT INTO A_OS VALUES (seq_OS.nextval, os_name, os_version);
+    if(trim(Ios_name) is not null and Ios_version>0) then
+      INSERT INTO A_OS VALUES (seq_OS.nextval, Ios_name, Ios_version);
     else
       RAISE_APPLICATION_ERROR(-20000, 'Chybne udaje OS pocitace.');
     end if;
   END new_os;
-  
-  
-  
+
+
+
   PROCEDURE new_computer (
-  bpcs_sessions IN VARCHAR2, comments IN VARCHAR2, create_time IN DATE, guarantees IN DATE, inventory_number IN NUMBER, last_update_time IN DATE, mac_address IN VARCHAR2, computer_name IN VARCHAR2, serial_number IN VARCHAR2,
-  computer_brand_maker IN VARCHAR2, compuer_model IN VARCHAR2,
-  extername_gpu IN NUMBER, optical_disk_drive IN NUMBER, hdd IN NUMBER, processor IN VARCHAR2, ram IN NUMBER,
-  os_name IN VARCHAR2, os_version IN NUMBER,
-  firstname IN VARCHAR2, lastname IN VARCHAR2, userrole IN VARCHAR2, userpassword IN VARCHAR2, login IN VARCHAR2, email IN VARCHAR2) AS    
-  
+  Ibpcs_sessions IN VARCHAR2, Icomments IN VARCHAR2, Icreate_time IN DATE, Iguarantees IN DATE, Iinventory_number IN VARCHAR2, Ilast_update_time IN DATE, Imac_address IN VARCHAR2, Icomputer_name IN VARCHAR2, Iserial_number IN VARCHAR2,
+  Icomputer_brand_maker IN VARCHAR2, Icompuer_model IN VARCHAR2,
+  Iextername_gpu IN NUMBER, Ioptical_disk_drive IN NUMBER, Ihdd IN NUMBER, Iprocessor IN VARCHAR2, Iram IN NUMBER,
+  Ios_name IN VARCHAR2, Ios_version IN NUMBER,
+  Ifirstname IN VARCHAR2, Ilastname IN VARCHAR2, Iuserrole IN VARCHAR2, Iuserpassword IN VARCHAR2, Ilogin IN VARCHAR2, Iemail IN VARCHAR2) AS    
+
   glob_maker NUMBER;
   glob_parameters NUMBER;
   glob_os NUMBER;
@@ -67,49 +68,50 @@ CREATE OR REPLACE PACKAGE BODY pocitacova_databaze_api AS
   glob_seq NUMBER;
 
     BEGIN  
-    
+
       BEGIN
-      select a_computer_brand.computer_brand_id into glob_maker from a_computer_brand where computer_brand_maker = a_computer_brand.computer_brand_maker and compuer_model=a_computer_brand.compuer_model;
+      select * into glob_maker from(select a_computer_brand.computer_brand_id from a_computer_brand where Icomputer_brand_maker = a_computer_brand.computer_brand_maker and Icompuer_model=a_computer_brand.compuer_model
+      ) where rownum=1;
         EXCEPTION
         WHEN NO_DATA_FOUND THEN
-            new_computer_brand(computer_brand_maker, compuer_model);
-            SELECT last_number into glob_maker FROM user_sequences WHERE sequence_name = 'seq_Brand';
+            new_computer_brand(Icomputer_brand_maker, Icompuer_model);
+            SELECT seq_Brand.currval into glob_maker FROM dual;
       END;
-      
+
       BEGIN
-      select a_computer_parameters.computer_parameters_id into glob_parameters from a_computer_parameters where extername_gpu = a_computer_parameters.extername_gpu 
-      and optical_disk_drive=a_computer_parameters.optical_disk_drive and hdd=a_computer_parameters.hdd and processor=a_computer_parameters.processor and ram=a_computer_parameters.ram;
+      select * into glob_parameters from(select a_computer_parameters.computer_parameters_id from a_computer_parameters where Iextername_gpu = a_computer_parameters.extername_gpu 
+      and Ioptical_disk_drive=a_computer_parameters.optical_disk_drive and Ihdd=a_computer_parameters.hdd and Iprocessor=a_computer_parameters.processor and Iram=a_computer_parameters.ram) where rownum = 1;
         EXCEPTION
         WHEN NO_DATA_FOUND THEN
-            new_computer_parameters(extername_gpu, optical_disk_drive, hdd, processor, ram);
-            SELECT last_number into glob_parameters FROM user_sequences WHERE sequence_name = 'seq_Parameters';
+            new_computer_parameters(Iextername_gpu, Ioptical_disk_drive, Ihdd, Iprocessor, Iram);
+            SELECT seq_Parameters.currval into glob_parameters FROM dual;
       END;
-      
+
       BEGIN
-      select a_os.os_id into glob_os from a_os where os_name = a_os.os_name and os_version=a_os.os_version;
+      select * into glob_os from(select a_os.os_id from a_os where Ios_name = a_os.os_name and Ios_version=a_os.os_version) where rownum = 1;
         EXCEPTION
         WHEN NO_DATA_FOUND THEN
-            new_os(os_name, os_version);
-            SELECT last_number into glob_os FROM user_sequences WHERE sequence_name = 'seq_OS';
+            new_os(Ios_name, Ios_version);
+            SELECT seq_OS.currval into glob_os FROM dual;
       END;
-      
+
       BEGIN
-      select a_user.user_id into glob_user from a_user where firstname = a_user.user_first_name and lastname=a_user.user_last_name and userrole=a_user.user_role and userpassword=a_user.user_password
-      and login=a_user.user_login and email=a_user.user_email_address;
+      select * into glob_user from (select a_user.user_id from a_user where Ifirstname = a_user.user_first_name and Ilastname=a_user.user_last_name and Iuserrole=a_user.user_role and Iuserpassword=a_user.user_password
+      and Ilogin=a_user.user_login and Iemail=a_user.user_email_address) where rownum=1;
         EXCEPTION
         WHEN NO_DATA_FOUND THEN
-            new_user(firstname, lastname, userrole, userpassword, login, email);
-            SELECT last_number into glob_user FROM user_sequences WHERE sequence_name = 'seq_User';
+            new_user(Ifirstname, Ilastname, Iuserrole, Iuserpassword, Ilogin, Iemail);
+            SELECT seq_User.currval into glob_user FROM dual;
       END;
-      
+
       BEGIN
        SELECT seq_Computer.nextval into glob_seq FROM dual;
       END;
-      
-      INSERT INTO A_COMPUTER VALUES (glob_seq, bpcs_sessions, comments, create_time, null, guarantees, inventory_number, last_update_time, mac_address, computer_name, 
-      serial_number, glob_maker, glob_parameters, glob_os, glob_user);
-      
+
+      INSERT INTO A_COMPUTER VALUES (glob_seq, Ibpcs_sessions, Icomments, Icreate_time, null, Iguarantees, Iinventory_number, Ilast_update_time, Imac_address, Icomputer_name, 
+      Iserial_number, glob_maker, glob_parameters, glob_os, glob_user);
+
   END new_computer;  
-  
+
 END pocitacova_databaze_api;
 /
---
+++ 11_vystup.sql
@@ -0,0 +1,78 @@
+--1) vytvoreni uzivatele
+
+begin
+pocitacova_databaze_api.new_user('Radim', 'Kratky', 'Manager', 'DLIo253BOQ3p', 'kratkra1', 'kratkra1@uhk.cz');
+end;
+
+select * from a_user where user_email_address='kratkra1@uhk.cz';
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+
+USER_ID 	USER_FIRST_NAME 	USER_LAST_NAME  	USER_ROLE	USER_PASSWORD		USER_LOGIN		USER_EMAIL_ADDRESS
+-------------------------------------------------------------------------------------------------------------------------------------------------
+2001		Radim			Kratky			Manager		DLIo253BOQ3p		kratkra1		kratkra1@uhk.cz
+
+
+
+
+
+--2) vytvoreni pocitace
+
+-- Computer brand, computer parameters, OS ani user jeste v databazi NEEXISTUJI
+--  -> procedura by je mìla sama vytvoøit
+-- Nový poèítaè:
+
+begin
+pocitacova_databaze_api.new_computer(
+'BPCS_1_FRYCZ-0800, BPCS_2_FRYCZ-0800, BPCS_3_FRYCZ-0800', 'Muj koment', TO_DATE('14.10.2000', 'DD.MM.YYYY'), TO_DATE('14.11.2009', 'DD.MM.YYYY'), 'FRYCZ - 800/813482H1:54:ZN:BH:WD:55', 
+TO_DATE('22.05.2019', 'DD.MM.YYYY'), 'H1:54:ZN:BH:WD:55', 'Zkusebni-FRYCZ-0800', '2BQGEQBP19JBK9APG',
+  'Lenovo', 'y510p',
+  '1', '1', '4096', 'Pentium', '16',
+  'Windows 10', '128',
+  'Jan', 'Vondracek', 'Manager', 'DLIo253BOQ3p', 'vondr1', 'vondr1@uhk.cz');
+end;
+
+
+SELECT * from a_computer where computer_name='Zkusebni-FRYCZ-0800';
+SELECT * from a_computer_brand where computer_brand_id=107;
+SELECT * from a_computer_parameters where computer_parameters_id=1202;
+SELECT * from a_os where os_id=42;
+SELECT * from a_user where user_id=2002;
+
+
+--Vytvoreny pocitac:
+
+COMPUTER_ID 	BPCS_SESSIONS        						COMMENTS        CREATE_T 	DISCARDED_TIME          GUARANTEES 	INVENTORY_NUMBER             		LAST_UPDATE_TIME            	MAC_ADDRESS             COMPUTER_NAME		SERIAL_NUMBER		COMPUTER_BRAND_ID 	COMPUTER_PARAMETERS_ID      	OS_ID    	USER_ID
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+10003		BPCS_1_FRYCZ-0800, BPCS_2_FRYCZ-0800, BPCS_3_FRYCZ-0800		Muj koment	14.10.00				14.11.09	FRYCZ - 800/813482H1:54:ZN:BH:WD:55	22.05.19			H1:54:ZN:BH:WD:55	Zkusebni-FRYCZ-0800	2BQGEQBP19JBK9APG	107			1202				42		2002
+
+
+
+--Vytvoreny vyrobce:
+
+COMPUTER_BRAND_ID	COMPUTER_BRAND_MAKER		COMPUER_MODEL
+-----------------------------------------------------------------------
+107			Lenovo				y510p
+
+
+
+--Vytvorene parametry:
+
+COMPUTER_PARAMETERS_ID		EXTERNAME_GPU		OPTICAL_DISK_DRIVE	HDD		PROCESSOR		RAM
+------------------------------------------------------------------------------------------------------------------------------
+1202				1			1			4096		Pentium			16
+
+
+
+--Vytvoreny OS:
+
+OS_ID		OS_NAME		OS_VERSION
+---------------------------------------------
+42		Windows 10	128
+
+
+
+--Vytvoreny uzivatel:
+
+USER_ID		USER_FIRST_NAME		USER_LAST_NAME		USER_ROLE	USER_PASSWORD		USER_LOGIN	USER_EMAIL_ADDRESS
+-------------------------------------------------------------------------------------------------------------------------------------------
+2002		Jan			Vondracek		Manager		DLIo253BOQ3p		vondr1		vondr1@uhk.cz
