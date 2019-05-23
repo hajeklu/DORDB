cba34bf40824e792aabe1f70b1f3882db6572cf3
manas08 <kratkra1@uhk.cz>
2019-05-23 19:35:57 +0200

    Object queries

--- 14_objektove_dotazy_z_bodu_4.sql
+++ 14_objektove_dotazy_z_bodu_4.sql
@@ -1,3 +1,5 @@
+spool '14_vysledky_dotazu.txt';
+set autotrace on;
 
 /*
  1. vypsat jména uživatelù, jejichž poèítaèe mají OS macOS (Object)
@@ -20,15 +22,18 @@ select c.user_id.user_first_name as Jmeno, c.user_id.user_last_name as Prijmeni,
 /*
  4. vypsat všechny parametry poèítaèe, jeho název, jméno uživatele, název OS, název výrobce a modelu, který je stále v záruce 2 let (object)
 */
-select * from (select c.computer_parameters_id.extername_gpu as GPU, c.computer_parameters_id.optical_disk_drive as OpticalDiskDrive,c.computer_parameters_id.hdd as HDD,c.computer_parameters_id.processor as Processor,
+select c.computer_parameters_id.extername_gpu as GPU, c.computer_parameters_id.optical_disk_drive as OpticalDiskDrive,c.computer_parameters_id.hdd as HDD,c.computer_parameters_id.processor as Processor,
 c.computer_parameters_id.ram as RAM,c.computer_name as Nazev, c.last_update_time as UpdateTime, c.user_id.user_first_name as Jmeno, c.user_id.user_last_name as Prijmeni, c.os_id.os_name as OS, 
 c.computer_brand_id.computer_brand_maker as Vyrobce, c.computer_brand_id.compuer_model as Model
-from obj_a_computer c  
-order by c.computer_name desc) where trunc(SYSDATE - 730) < last_update_time;
+from obj_a_computer c  where SYSDATE < c.guarantees
+order by c.computer_name desc;
 
 
 /*
- 5. Vypsani všech poèítaèù a seøazení podle poètu uživatelù (object)
+ 5. Vypsani všech poèítaèù a seøazení podle poètu uživatelù - prvnich 20 (object)
 */
-select c.user_id.user_first_name as Jméno, c.user_id.user_last_name as Pøíjmení, count(c.computer_id) as Poèet_poèítaèù from obj_a_computer c
-group by c.user_id.user_first_name, c.user_id.user_last_name order by Poèet_poèítaèù desc;
+SELECT * FROM (select c.user_id.user_first_name as Jméno, c.user_id.user_last_name as Pøíjmení, count(c.computer_id) as Poèet_poèítaèù from obj_a_computer c
+group by c.user_id.user_first_name, c.user_id.user_last_name order by Poèet_poèítaèù desc)where rownum < 21;
+
+set autotrace off;
+spool off;
