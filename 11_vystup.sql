f9d70afa12611a297222425730bdc5be69d92e0d
manas08 <kratkra1@uhk.cz>
2019-05-23 11:38:39 +0200

    Add SPOOL outputs and query outputs

--- 11_vystup.sql
+++ 11_vystup.sql
@@ -1,26 +1,17 @@
---1) vytvoreni uzivatele
+-- Testovaci skript
 
+spool '11_vystup_spool.txt';
+
+--1) vytvoreni uzivatele
 begin
 pocitacova_databaze_api.new_user('Radim', 'Kratky', 'Manager', 'DLIo253BOQ3p', 'kratkra1', 'kratkra1@uhk.cz');
 end;
-
-select * from a_user where user_email_address='kratkra1@uhk.cz';
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
-
-USER_ID 	USER_FIRST_NAME 	USER_LAST_NAME  	USER_ROLE	USER_PASSWORD		USER_LOGIN		USER_EMAIL_ADDRESS
--------------------------------------------------------------------------------------------------------------------------------------------------
-2001		Radim			Kratky			Manager		DLIo253BOQ3p		kratkra1		kratkra1@uhk.cz
-
-
-
-
+/
 
 --2) vytvoreni pocitace
-
 -- Computer brand, computer parameters, OS ani user jeste v databazi NEEXISTUJI
 --  -> procedura by je mìla sama vytvoøit
 -- Nový poèítaè:
-
 begin
 pocitacova_databaze_api.new_computer(
 'BPCS_1_FRYCZ-0800, BPCS_2_FRYCZ-0800, BPCS_3_FRYCZ-0800', 'Muj koment', TO_DATE('14.10.2000', 'DD.MM.YYYY'), TO_DATE('14.11.2009', 'DD.MM.YYYY'), 'FRYCZ - 800/813482H1:54:ZN:BH:WD:55', 
@@ -30,6 +21,23 @@ TO_DATE('22.05.2019', 'DD.MM.YYYY'), 'H1:54:ZN:BH:WD:55', 'Zkusebni-FRYCZ-0800',
   'Windows 10', '128',
   'Jan', 'Vondracek', 'Manager', 'DLIo253BOQ3p', 'vondr1', 'vondr1@uhk.cz');
 end;
+/
+
+spool off;
+
+
+
+
+--Overeni spravnosti:
+
+select * from a_user where user_email_address='kratkra1@uhk.cz';
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+
+USER_ID 	USER_FIRST_NAME 	USER_LAST_NAME  	USER_ROLE	USER_PASSWORD		USER_LOGIN		USER_EMAIL_ADDRESS
+-------------------------------------------------------------------------------------------------------------------------------------------------
+2001		Radim			Kratky			Manager		DLIo253BOQ3p		kratkra1		kratkra1@uhk.cz
+
+
 
 
 SELECT * from a_computer where computer_name='Zkusebni-FRYCZ-0800';
